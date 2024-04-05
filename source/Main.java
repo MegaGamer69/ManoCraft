3702f1e0dc525ebe30013d2c5961aa4f4897f552
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-04-05 01:05:22 -0300

    Create a New Main.java

---
+++ source/Main.java
@@ -0,0 +1,17 @@
+package;
+
+// Importações Úteis
+import javafx.graphics.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+
+// a Classe Principal para o Jogo
+public class Main
+{
+    @Override
+    // a Função Inicial
+    public void start(Stage stage)
+    {
+        
+    }
+}
