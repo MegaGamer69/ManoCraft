358877a70931b67179deaf9eb5aa3e13ca2a19dd
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-04-05 01:08:36 -0300

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -6,12 +6,13 @@ import javafx.stage.*;
 import javafx.scene.*;
 
 // a Classe Principal para o Jogo
-public class Main
+public class Main extends Application
 {
     @Override
     // a Função Inicial
     public void start(Stage stage)
     {
-        
+        Group root = new Group();
+        Scene scene = new Scene(200, 200);
     }
 }
