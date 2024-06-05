12e7cc8ffdb6c215eb86cd6d0204b84b644c9059
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-06-05 00:39:02 -0300

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -5,11 +5,8 @@
 // a Classe Principal para o Jogo
 public class Main
 {
-    // o Manipulador da Janela
-    private long window;
-    
     // Chamado quando a Aplicação é Executada
-    public void run()
+    public static void main(String[] args)
     {
         System.out.println("Executando o Jogo...");
     }
