48328cf3c0a806c36c3b5fe5da3eb2906425befb
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-06-05 00:23:10 -0300

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -1,16 +1,3 @@
-// Importações Úteis
-import org.lwjgl.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import static org.lwjgl.glfw.CallBacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
-import Block.*;
-
 // Direitos Autorais:
 // @MegaGamer69(Criador do Código Fonte)
 // 2024 ManoCraft
