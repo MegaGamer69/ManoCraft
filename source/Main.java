4b5b2dc703bb92084381a2e5034d9077f28409a8
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-06-16 02:26:33 -0300

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -2,12 +2,28 @@
 // @MegaGamer69(Criador do Código Fonte)
 // 2024 ManoCraft
 
+// Importações Úteis
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+// Importações Estáticas Úteis
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 // a Classe Principal para o Jogo
 public class Main
 {
+    // o Manipulador da Janela
+    private long window;
+    
     // Chamado quando a Aplicação é Executada
-    public static void main(String[] args)
+    public void run()
     {
-        System.out.println("Executando o Jogo...");
+        // não tem Nada para Fazer, por Enquanto...
     }
 }
