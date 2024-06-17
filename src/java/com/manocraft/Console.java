04ab9b188798f2ee6bb764b1dcd9063334a3f3bb
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-06-16 21:44:48 -0300

    Update Console.java

--- src/java/com/manocraft/Console.java
+++ src/java/com/manocraft/Console.java
@@ -7,13 +7,12 @@ package com.manocraft;
 
 // Importações Concretas Úteis do Java
 import java.nio.*;
-import java.util.ArrayList.*;
-import java.util.List.*;
+import java.util.ArrayList;
+import java.util.List;
 
-// Importações Estáticas ùteis do LWJGL
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.glfw.GLFWCharCallback.*;
-import static org.lwjgl.glfw.GLFWKeyCallback.*;
+// Importações Concretas Úteis do LWJGL
+import org.lwjgl.glfw.GLFWCharCallback.*;
+import org.lwjgl.glfw.GLFWKeyCallback.*;
 
 // Classe Responsavel por Controlar o Console
 public class Console
@@ -22,19 +21,19 @@ public class Console
     public List<String> commandHistory = new ArrayList<>();
 
     // uma Lista de Histórico das Mensagens
-    public List<String> messageHistory = new ArrayList<>().
+    public List<String> messageHistory = new ArrayList<>();
 
     // Determina a Visibilidade do Console
-    public boolean visibility = false;
+    public boolean visibility;
 
     // Define o Comando Atual
     public String command = "Console/> ";
     
     // Cria uma nova Instância do Console
-    public Console(long window, boolean visible = false)
+    public Console(long window, boolean visible)
     {
         // Verifica se o Manipulador da Janela está Nula
-        if(window == null)
+        if(window == 0)
         {
             System.out.println("Não é Possivel usar o Console, o Manipulador da Janela não Está Definido");
         }
@@ -46,6 +45,9 @@ public class Console
     // Escreve a Mensagem
     public void write(String message)
     {
+        // Imprima a Mensagem
+        System.out.print(message);
+        
         // Esvazie a Entrada de Comando
         this.command = "Console/> ";
     }
