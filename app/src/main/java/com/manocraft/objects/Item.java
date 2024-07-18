59c318f397aca223d7d55fe02d11d91f22231f2a
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-07-18 17:18:30 -0300

    Update Item.java

--- app/src/main/java/com/manocraft/objects/Item.java
+++ app/src/main/java/com/manocraft/objects/Item.java
@@ -1,2 +1,25 @@
 // o Pacote do Código
 package com.manocraft.objects;
+
+// Direitos Autorais:
+// MegaGamer69(Criador do Código Fonte)
+// 2024 ManoCraft
+
+// Importações Estáticas Úteis do LWJGL
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.Callbacks.*;
+
+// o Item é Criado e Registrado para poder Entrar no Jogo, tenha Certeza do Ícone do Item ter uma Resolução Baixa
+// Normalmente, os Ícones são Criados numa Escala de 32 por 32 Pixels
+public class Item
+{
+    // Váriaveis Úteis
+    private String itemName;
+    private Strind itemID;
+
+    // Cria uma Nova Intância do Item
+    public Item(String name, String identifier)
+    {
+        
+    }
+}
